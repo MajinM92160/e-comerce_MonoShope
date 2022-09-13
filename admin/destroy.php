f5611d7ba1457f1eb76e2d7f296785f0890cac4b
unknown <mkacem6@outlook.com>
2022-09-13 08:28:37 +0200

    projet E-comShop site e-commerce de vente de sneakers fin

--- admin/destroy.php
+++ admin/destroy.php
@@ -1,6 +1,7 @@
 <?php
 session_start();
 
+/* ici ce if signifie si la session qui porte ce nom xRttpHo0greL39 à été set alors je supprime tout en recevant un tableau vide en déclarant un array vide et je detruit la session grace à la fonction session destroy et je renvoie donc l'utilisateur vers la page index.php */
 if (isset($_SESSION['xRttpHo0greL39'])){
 
     $_SESSION['xRttpHo0greL39'] = array();
